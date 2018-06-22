b069bcbc9ba297ba52f64feb1e8ddd945e54256a
Kaz Utashiro <kaz@utashiro.com>
2018-06-22 13:45:40 +0900

    minil init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Greple::msdoc - It's new $module
+
+# SYNOPSIS
+
+    use App::Greple::msdoc;
+
+# DESCRIPTION
+
+App::Greple::msdoc is ...
+
+# LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kaz Utashiro <kaz@utashiro.com>
---
+++ lib/App/Greple/msdoc.pm
@@ -0,0 +1,39 @@
+package App::Greple::msdoc;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::msdoc - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::msdoc;
+
+=head1 DESCRIPTION
+
+App::Greple::msdoc is ...
+
+=head1 LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kaz Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
