c1ad31a1cc2e1a596289ee5fe1ea3703784d1335
Kazumasa Utashiro <kaz@utashiro.com>
2022-10-08 13:22:11 +0900

    update test with updated runner submodule

--- t/Util.pm
+++ t/Util.pm
@@ -4,6 +4,31 @@ use utf8;
 use open IO => ':utf8', ':std';
 
 use lib 't/runner';
-use Greple;
+use Runner qw(get_path);
+
+my $greple_path = get_path('greple', 'App::Greple') or die Dumper \%INC;
+
+sub greple {
+    Runner->new($greple_path, @_);
+}
+
+sub run {
+    greple(@_)->run;
+}
+
+sub msdoc {
+    greple '-Mmsdoc', @_;
+}
+
+sub slurp {
+    my $file = shift;
+    open my $fh, "<:utf8", $file or die "open: $!";
+    do { local $/; <$fh> };
+}
+
+sub line {
+    my($text, $line, $comment) = @_;
+    like($text, qr/\A(.*\n){$line}\z/, $comment//'');
+}
 
 1;
